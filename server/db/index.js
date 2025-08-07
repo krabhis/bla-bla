d5c2b5843af1f53103b6472d9ec2ae853a115eb5
krabhis <abhishekkr212402@gmail.com>
2025-08-07 19:26:31 +0530

    added server code and connected database

---
+++ server/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+import {DB_NAME} from "../constant.js"
+
+const connectDB = async ()=>{
+    try {
+        const response = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log("MongoDB connected ...");
+    } catch (error) {
+        console.error("Error connecting t0 mongoDB\n\n",error);
+        process.exit(1);
+        
+    }
+};
+
+export default connectDB;
